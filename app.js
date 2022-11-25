e0ed15a9ba00dc8bacc76ca25945e4ab28c872b1
Kayleigh <57508868+gordokay@users.noreply.github.com>
2022-11-25 16:31:59 -0500

    Add button functionality

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 function add(a, b) {
-  return a + b;
+  return Math.round((a + b) * 100) / 100;
 }
 
 function subtract(a, b) {
-  return a - b;
+  return Math.round((a - b) * 100) / 100;
 }
 
 function multiply(a, b) {
-  return a * b;
+  return Math.round((a * b) * 100) / 100;
 }
 
 function divide(a, b) {
   if(b === 0) {
     return 'ERROR: divide by zero';
   }
-  return a / b;
+  return Math.round((a / b) * 100) / 100;
 }
 
 function operate(op, a, b) {
@@ -30,8 +30,16 @@ function operate(op, a, b) {
   }
 }
 
+//returns true if operate is called so display can be updated with result
 function updateOperation(operand, operator, operation) {
-  operation.push(operand, operator);
+  operation.push(Number(operand), operator);
+  if(operation.length == 4) {
+    let a = operation[0];
+    let op = operation[1];
+    let result = operate(op, a, Number(operand));
+    operation.splice(0, 3, result);
+    return true;
+  }
 }
 
 function updateDisplay(element, display, willClear) {
@@ -45,16 +53,21 @@ function clearDisplay(display) {
   display.textContent = '';
 }
 
+function toggleOperators(operators) {
+  operators.forEach(operator => operator.disabled = !operator.disabled);
+}
+
 function makeCalculator() {
   const display = document.querySelector('#display');
   const digits = document.querySelectorAll('.digit');
   const operators = document.querySelectorAll('.operator');
   const decimal = document.querySelector('#decimal')
   const equals = document.querySelector('#equals');
-  const clear = document.querySelectorAll('#clear');
+  const clear = document.querySelector('#clear');
 
   const operation = [];
 
+  let operatorsEnabled = true;
   //determines if display will clear on next update
   let willClear = false;
 
@@ -63,12 +76,50 @@ function makeCalculator() {
     if(willClear) {
       willClear = false;
     }
+    if(!operatorsEnabled) {
+      toggleOperators(operators);
+      operatorsEnabled = true;
+    }
   }));
 
   operators.forEach(operator => operator.addEventListener('click', () => {
-    updateOperation(operator.textContent, display.textContent, operation);
     willClear = true;
+    if(decimal.disabled) {
+      decimal.disabled = false;
+    }
+    if(updateOperation(display.textContent, operator.textContent, operation)) {
+      updateDisplay(operation[0], display, willClear);
+    }
+    toggleOperators(operators);
+    operatorsEnabled = false;
   }))
+
+  decimal.addEventListener('click', () => {
+    decimal.disabled = true;
+    updateDisplay('.', display, willClear)
+    if(willClear) {
+      willClear = false;
+    }
+    if(!operatorsEnabled) {
+      toggleOperators(operators);
+      operatorsEnabled = true;
+    }
+  })
+
+  equals.addEventListener('click', () => {
+    willClear = true;
+    operation.push(Number(display.textContent));
+    let result = operate(operation[1], operation[0], operation[2]);
+    operation.splice(0);
+    updateDisplay(result, display, willClear);
+  })
+
+  clear.addEventListener('click', () => {
+    operation.splice(0);
+    display.textContent = '';
+    willClear = false;
+    operatorsEnabled = true;
+  })
 }
 
 makeCalculator();
